ea68eec865beb62be465d4ca49f5b52877b9f143
Roey <roitamano@gmail.com>
2024-10-09 16:31:40 +0300

    donator

--- donator.py
+++ donator.py
@@ -1,13 +1,16 @@
 import consts
 import validation
+import openpyxl as op
 
 
 def donation():
+    wb = op.load_workbook(r'gear_amounts.xlsx')
     gear = input("what do you want to donate?")
     if gear in consts.MIN_GEAR_DICT:
         bases_lack = []
         for base in consts.BASE_CODES_DICT:
-            if("alisa") < consts.MIN_GEAR_DICT[gear][base]:
+            ws = wb[base]
+            if ws.cell(row=, column=2).value < consts.MIN_GEAR_DICT[gear][base]:
                 bases_lack.append(base)
         print("the red ones are the most urgent")
         for basee in bases_lack:
@@ -21,4 +24,19 @@ def donation():
             amount = int(input("how many do you want to donate?"))
             gmail = input("what is your email?")
             if validation.checking_mail(gmail):
-                pass# send the donator details
+                pass# send the donor details
+
+
+def check_row(gear):
+    if gear == "Vests":
+        row = consts.VESTS_ROW
+    elif gear == "Gloves":
+        row = consts.GLOVES_ROW
+    elif gear == "Helmets":
+        row = consts.HELMETS_ROW
+    elif gear == "Goggles":
+        row = consts.GOGGLES_ROW
+    elif gear == "Food":
+        row = consts.FOOD_ROW
+    return row
+
