9802a67cf923aaa0fb26daa0fd2a882f82a9d521
jbt <noya.weinberg6340@gmail.com>
2024-10-09 14:32:24 +0300

    Initial consts (from plan)

---
+++ consts.py
@@ -0,0 +1,53 @@
+email_dict = {
+    "Uvda": "Uvda@gmail.com",
+    "Gidonim": "Gidonim@gmail.com",
+    "Hatzor": "Hatzor@gmail.com",
+    "Nevatim": "Nevatim@gmail.com",
+    "Palmahim": "Palmahim@gmail.com"
+}
+
+base_codes_dict = {
+    "Uvda": "11",
+    "Gidonim": "22",
+    "Hatzor": "33",
+    "Nevatim": "44",
+    "Palmahim": "55"
+}
+
+min_gear_dict = {
+    "Vests": {
+        "Uvda": 11,
+        "Gidonim": 22,
+        "Hatzor": 33,
+        "Nevatim": 44,
+        "Palmahim": 55
+    },
+    "Gloves": {
+        "Uvda": 11,
+        "Gidonim": 22,
+        "Hatzor": 33,
+        "Nevatim": 44,
+        "Palmahim": 55
+    },
+    "Helmets": {
+        "Uvda": 11,
+        "Gidonim": 22,
+        "Hatzor": 33,
+        "Nevatim": 44,
+        "Palmahim": 55
+    },
+    "Goggles": {
+        "Uvda": 11,
+        "Gidonim": 22,
+        "Hatzor": 33,
+        "Nevatim": 44,
+        "Palmahim": 55
+    },
+    "Food": {
+        "Uvda": 11,
+        "Gidonim": 22,
+        "Hatzor": 33,
+        "Nevatim": 44,
+        "Palmahim": 55
+    }
+}
