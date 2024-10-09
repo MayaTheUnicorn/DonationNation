c415b7cd24781fd86fb511b1fab05c19ee526f2c
jbt <noya.weinberg6340@gmail.com>
2024-10-09 16:06:21 +0300

    path update

--- alisa.scratch.py
+++ alisa.scratch.py
@@ -1,10 +1,9 @@
 import openpyxl as op
 
 
-wb = op.load_workbook(r'C:\Users\kotbe\OneDrive\Desktop\Donation_Nation.xlsx')
-ws = wb['Sheet1']
-# print(ws['B2'].value)
-ws['B2'].value = 5
-wb.save(r'C:\Users\kotbe\OneDrive\Desktop\Donation_Nation.xlsx')
-#
-# print(ws.cell(row=2, column=3).value)
+wb = op.load_workbook(r'gear_amounts.xlsx')
+ws = wb['Uvda']
+print(ws['B2'].value)
+# ws['B2'].value = 5
+# wb.save(r'C:\Users\kotbe\OneDrive\Desktop\Donation_Nation.xlsx')
+# # print(ws.cell(row=2, column=3).value)
