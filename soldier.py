9fbe7f0870b1dcc75a27bff5e6e5df953656b1cf
MayaTheUnicorn <shmayaf11@gmail.com>
2024-10-10 11:18:34 +0300

    soldier_login func take list instead of two inputs

--- soldier.py
+++ soldier.py
@@ -3,7 +3,8 @@ import consts
 
 
 # Check if soldier entered correct password
-def soldier_login(base_choice, base_code):
+def soldier_login(choice_base_code):
+    base_choice, base_code = choice_base_code
     if consts.BASE_CODES_DICT[base_choice] == base_code:
         return True
     else:
