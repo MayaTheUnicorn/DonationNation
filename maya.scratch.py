3f5debad0f65ce0dd938255fba6bf41ca2d4dc57
MayaTheUnicorn <shmayaf11@gmail.com>
2024-10-10 15:42:15 +0300

    עיצוב

--- maya.scratch.py
+++ maya.scratch.py
@@ -1,10 +1,21 @@
+import consts
 from screen import *
 import tkinter
 from PIL import Image, ImageTk
 
+def create_welcome_screen_1(screen, donor_screen, soldier_screen):
+    frame = tk.Frame(width=consts.WINDOW_WIDTH, height=consts.WINDOW_HEIGHT)
+
+    tkinter.Label(frame, text="Welcome to DonationNation!", font=consts.FONT_TUPLE, fg="green").place(relx=0.2, rely=0.1)
+    tkinter.Label(frame, text="Are you a soldier or a donor?", font=consts.FONT_TUPLE, fg="green").place(relx=0.18, rely=0.3)
+    tkinter.Button(frame, text="Soldier", command=lambda: show_frame(soldier_screen), font=consts.FONT_TUPLE).place(relx=0.18, rely=0.5)
+    tkinter.Button(frame, text="Donor", command=lambda: show_frame(donor_screen), font=consts.FONT_TUPLE, bg="green").place(relx=0.4, rely=0.5)
+
+    return frame
+
 def main():
     root = tk.Tk()
-    root.geometry("400x400")
+    root.geometry("800x800")
     root.title("DonationNation")
 
     # Create screens
@@ -14,7 +25,7 @@ def main():
     details_screen = create_details_screen(root, thank_you_screen)
     donor_screen = create_donor_screen(root, details_screen, wrong_screen)
     soldier_screen = create_soldier_screen(root, action_soldier_screen)
-    welcome_screen = create_welcome_screen(root, donor_screen, soldier_screen)
+    welcome_screen = create_welcome_screen_1(root, donor_screen, soldier_screen)
 
     # Add logo
     logo = Image.open("media/logo.png")
