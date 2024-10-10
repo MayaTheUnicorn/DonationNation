91ef66a35a4ebba453905c1de2ca2e151ba6bee6
MayaTheUnicorn <shmayaf11@gmail.com>
2024-10-10 15:05:02 +0300

    main with logo

--- maya.scratch.py
+++ maya.scratch.py
@@ -1,33 +1,39 @@
-import tkinter as tk
-from tkinter import OptionMenu, StringVar
-from tkinter import messagebox
+from screen import *
+import tkinter
+from PIL import Image, ImageTk
+
+def main():
+    root = tk.Tk()
+    root.geometry("400x400")
+    root.title("DonationNation")
+
+    # Create screens
+    wrong_screen = wrong(root)
+    action_soldier_screen = create_action_soldier_screen(root)
+    thank_you_screen = create_thank_you_screen(root)
+    details_screen = create_details_screen(root, thank_you_screen)
+    donor_screen = create_donor_screen(root, details_screen, wrong_screen)
+    soldier_screen = create_soldier_screen(root, action_soldier_screen)
+    welcome_screen = create_welcome_screen(root, donor_screen, soldier_screen)
+
+    # Add logo
+    logo = Image.open("media/logo.png")
+    logo = logo.resize((100, 100), Image.Resampling.LANCZOS)
+    photo_image = ImageTk.PhotoImage(logo)
+    tkinter.Label(root, image=photo_image).place(relx=0.7, rely=0.7)
+
+    # Add screens to the main window
+    welcome_screen.grid(row=0, column=0, sticky="nsew")
+    donor_screen.grid(row=0, column=0, sticky="nsew")
+    soldier_screen.grid(row=0, column=0, sticky="nsew")
+    details_screen.grid(row=0, column=0, sticky="nsew")
+    thank_you_screen.grid(row=0, column=0, sticky="nsew")
+    action_soldier_screen.grid(row=0, column=0, sticky="nsew")
+    wrong_screen.grid(row=0, column=0, sticky="nsew")
+    # Show welcome screen
+    show_frame(welcome_screen)
+
+    root.mainloop()
 
 
-def done():
-    print("yay")
-
-
-def OnClick_Submit():
-    base_name = my_menu.get()
-    messagebox.showinfo("Captured Data", base_name)
-
-
-root = tk.Tk()
-root.geometry("400x400")
-root.title("test")
-
-# drop down boxes
-clicked = StringVar()
-clicked.set("choose day")
-
-my_menu = OptionMenu(root, clicked, "Monday", "Tuesday", "Wednesday")
-my_menu.pack()
-
-button = tk.Button(root, text="done", command=done)
-
-submit_button = tk.Button(root, text='Submit', command=OnClick_Submit)
-submit_button.pack(anchor=tk.W, padx=10)
-
-
-root.mainloop()
-
+main()
